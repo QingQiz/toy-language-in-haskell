2448b7b4957e78978ac174f71dccd86f536e7e92
QingQiz <sofeeys@outlook.com>
2020-04-16 00:32:32 +0800

    feat: loop stmt, assign stmt, array

--- src/Grammar.hs
+++ src/Grammar.hs
@@ -14,6 +14,10 @@ data Ast = Empty
          | UnaryNode Op Ast
          | IfStmt Ast Ast Ast -- ifstmt expr stmt else_stmt
          | StmtList [Ast]
+         | ForStmt Ast Ast Ast Ast -- forstmt beg cond step loop_stmt
+         | DoStmt Ast Ast -- dostmt loop_stmt cond
+         | Array Ast Ast -- array id index
+         | Assign Ast Ast -- assign left right
 
 data Op = Not
         | Mul | Div
@@ -47,7 +51,12 @@ instance Show Ast where
     show (Identifier p) = show p
     show (BinNode op l r) = "(" ++ show op ++ " " ++ show l ++ " " ++ show r++ ")"
     show (UnaryNode op p) = "(" ++ show op ++ " " ++ show p ++ ")"
-    show (IfStmt c s es) = "(if (" ++ show c ++ ") (" ++ show s ++ ") (" ++ show es ++ "))"
+    show (IfStmt c s es) = "(if " ++ show c ++ " ; " ++ show s ++ " ; " ++ show es ++ ")"
+    show (StmtList ss) = show ss
+    show (ForStmt b c s l) = "(for " ++ show b ++ " ; " ++ show c ++ " ; " ++ show s ++ " ; " ++show l ++ ")"
+    show (DoStmt l c) = "(do " ++ show l ++ " ; " ++ show c ++ ")"
+    show (Array a b) = show a ++ "["++ show b ++"]"
+    show (Assign a b) = show a ++ "=" ++ show b
 
 
 ----------------------------------------------------------
@@ -59,7 +68,6 @@ ident  = Identifier <$> id_parser where
         fo <- many (letter <|> digit <|> char '_')
         return $ f1 ++ fo
 
-
 str :: Parser Ast
 str = fmap Str $ skipMany space >> between (char '"') valid (char '"') where
     valid = many $ satisfy (\inp ->
@@ -67,6 +75,19 @@ str = fmap Str $ skipMany space >> between (char '"') valid (char '"') where
             if o == 32 || o == 33 || (35 <= o && o <= 126)
             then True
             else False)
+
+array :: Parser Ast
+array = do
+    l <- ident
+    r <- between (many space >> char '[') expr (many space >> char ']')
+    return $ Array l r
+
+assign :: Parser Ast
+assign = do
+    l <- array <|> ident
+    many space >> char '='
+    r <- expr
+    return $ Assign l r
 
 
 ----------------------------------------------------------
@@ -99,29 +120,53 @@ unary_expr :: Parser Ast
 unary_expr = unaryOpChain (pUnaryValue char '!' Not) term
 
 term :: Parser Ast
-term = (Number <$> integer <|> between (char '(') expr (char ')')) <|> ident
+term = (Number <$> integer <|> between (char '(') expr (char ')')) <|>
+       array <|> ident
+
 
 ----------------------------------------------------------
 -- stmt
 stmt :: Parser Ast
-stmt = if_stmt <|> (nothing <* (many space >> char ';'))
+stmt = if_stmt <|> stmt_list <|> loop_stmt  <|>
+      (nothing <* (many space >> char ';')) <|> -- empty
+      (assign  <* (many space >> char ';')) -- assig_stmt
 
 nothing :: Parser Ast
-nothing =
-    string "" >> return Empty
+nothing = string "" >> return Empty
 
 stmt_list :: Parser Ast
-stmt_list = undefined
-
+stmt_list = fmap StmtList $ between open valid close where
+    open  = many space >> char '{'
+    close = many space >> char '}'
+    valid = many (many space >> stmt)
+    
 if_stmt :: Parser Ast
 if_stmt = do
     many space >> string "if"
-    cond <- many space >> between (char '(') expr (char ')')
+    cond <- many space >> between (char '(') expr (many space >> char ')')
     cond_stmt <- many space >> stmt
     else_stmt <- many space >> ((string "else" >> stmt) <|> nothing)
     return $ IfStmt cond cond_stmt else_stmt
 
-
+loop_stmt :: Parser Ast
+loop_stmt = for_stmt <|> do_stmt where
+    for_stmt = do
+        many space >> string "for" >> many space >> char '('
+        a1 <- assign
+        many space >> char ';'
+        c  <- expr
+        many space >> char ';'
+        a2 <- assign
+        many space >> char ')'
+        s  <- stmt
+        return $ ForStmt a1 c a2 s
+    do_stmt  = do
+        many space >> string "do"
+        s <- stmt
+        many space >> string "while" >> many space >> char '('
+        e <- expr
+        many space >> char ')'
+        return $ DoStmt s e
 
 
 ----------------------------------------------------------
