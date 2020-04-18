d8867babf451c00d928b134e6bd86e7a3ee77240
QingQiz <sofeeys@outlook.com>
2020-04-19 02:42:14 +0800

    feat: mostly done for semantic analysis

--- Libs/Semantic.hs
+++ Libs/Semantic.hs
@@ -4,6 +4,7 @@ import Grammar
 import TransAst
 import ParserAst
 
+import Data.Semigroup hiding ((<>))
 import Data.Map as Map hiding (foldl, map)
 
 -- TODO insert reserve name into empty symbol table
@@ -21,13 +22,13 @@ transConst [] st = Just st
 transConst (ConstDef t dfs:cs) st =
     transConstDef t dfs st >>= transConst cs where
         transConstDef :: Type -> [(Ast, Ast)] -> SymbolTable -> Maybe SymbolTable
-        transConstDef TInt ((Identifier i, Number _):dfs) st =
+        transConstDef TInt ((Identifier i, Number n):dfs) st =
             case Map.lookup i st of
-                Nothing -> transConstDef TInt dfs $ Map.insert i (SConst SInt) st
+                Nothing -> transConstDef TInt dfs $ Map.insert i (SConst SInt n) st
                 _       -> Nothing -- redef
-        transConstDef TChar ((Identifier i, Ch _):dfs) st =
+        transConstDef TChar ((Identifier i, Ch c):dfs) st =
             case Map.lookup i st of
-                Nothing -> transConstDef TChar dfs $ Map.insert i (SConst SChar) st
+                Nothing -> transConstDef TChar dfs $ Map.insert i (SConst SChar (ord c)) st
                 _       -> Nothing -- redef
         transConstDef _ [] st = Just st
         transConstDef _ _ _ = Nothing -- wrong type , we can transform it to correct type
@@ -64,10 +65,12 @@ transFunc (f:fs) st = transAFun f st >>> transFunc fs where
     transAFun (FuncDef ft (Identifier fn) pl fb) st =
         case Map.lookup fn st of
             -- function params will replace global vars of the same name
-            Nothing -> case transParamList pl (Map.insert fn STempSymbol empty_st) of
+            Nothing -> case transParamList pl empty_st of
                 Just nst -> let fs = pSFun ft pl
-                                nst' = Map.insert fn fs (Map.union nst st)
-                            in case transComdStmt fb nst' of
+                                -- param list can cover function name
+                                nst' = Map.union nst (Map.insert fn fs st)
+                                -- insert ("", SFunction) to symbol table to ckeck ret_stmt
+                            in case transComdStmt fb (Map.insert "" fs nst') of
                                 Just ast -> Just (ast, Map.insert fn fs st)
                                 _        -> Nothing
                 Nothing  -> Nothing
@@ -77,7 +80,7 @@ transFunc (f:fs) st = transAFun f st >>> transFunc fs where
     transParamList ((t, Identifier i):pls) st = step (t, i) st >>= transParamList pls where
         step (t, i) st = case Map.lookup i st of
             Nothing -> Just $ Map.insert i (SVariable $ transType t) st
-            _       -> Nothing
+            _       -> Nothing -- same name in param list
     transParamList [] st = Just st
 
     pSFun :: FunType -> [(Type, Ast)] -> Symbol
@@ -93,7 +96,188 @@ transComdStmt (ComdStmt cs vs sl) st =
 
 
 transStmtList :: Ast -> SymbolTable -> Maybe Ast
-transStmtList =  undefined
+transStmtList (StmtList stmts) st = toStmtList $ transStmts stmts [] where
+    toStmtList :: Maybe [Ast] -> Maybe Ast
+    toStmtList (Just x) = Just $ StmtList x
+    toStmtList Nothing  = Nothing
+
+    transStmts :: [Ast] -> [Ast] -> Maybe [Ast]
+    transStmts [] r = Just r
+    transStmts (stmt:stmts) zero = case transAStmt stmt of
+        Nothing -> Nothing
+        Just a  -> transStmts stmts $ a:zero
+
+    transAStmt :: Ast -> Maybe Ast
+    transAStmt (IfStmt c s es)   = transIfStmt c s es
+    transAStmt sl@(StmtList _)   = transStmtList sl st
+    transAStmt (ForStmt s e a b) = transForStmt s e a b
+    transAStmt (DoStmt b c)      = transDoStmt b c
+    transAStmt (Rd ids)          = transReadStmt ids
+    transAStmt (Wt s e)          = transWriteStmt s e
+    transAStmt (Ret e)           = transRetStmt e
+    transAStmt (FuncCall n pl)   = transFunCall n pl
+    transAStmt (Empty)           = Just Empty
+    transAStmt as@(Assign _ _)   = transAssign as
+
+    --             cond  stmt else-stmt
+    transIfStmt :: Ast -> Ast -> Ast -> Maybe Ast
+    transIfStmt c s es = toIfStmt $ case transExpr c of
+        -- we can check t::SType for cond later
+        Just (t, a) -> Just a <> transAStmt s <> transAStmt es <> Just []
+        _           -> Nothing
+        where
+            toIfStmt Nothing = Nothing
+            toIfStmt (Just [a, b, c]) = Just $ IfStmt a b c
+
+    --             start   end   update  body 
+    transForStmt :: Ast -> Ast -> Ast -> Ast -> Maybe Ast
+    transForStmt s e u b = toForStmt $ case transExpr e of
+        -- same to if stmt
+        Just (t, a) -> transAssign s <> Just a <> transAssign u <> transAStmt b <> Just []
+        _           -> Nothing
+        where
+            toForStmt Nothing = Nothing
+            toForStmt (Just [a, b, c, d]) = Just $ ForStmt a b c d
+
+    --             stmt   cond
+    transDoStmt :: Ast -> Ast -> Maybe Ast
+    transDoStmt s c = toDoStmt $ case transExpr c of
+        Nothing -> Nothing
+        Just (t, a) -> transAStmt s <> Just a <> Just []
+        where
+            toDoStmt Nothing = Nothing
+            toDoStmt (Just [a, b]) = Just $ DoStmt a b
+
+    transReadStmt :: [Ast] -> Maybe Ast
+    transReadStmt rd = toReadStmt $ for_rest rd where
+        for_rest (Identifier x:xs) = check x <> for_rest xs
+        for_rest [] = Just []
+
+        check x = case Map.lookup x st of
+            Just (SVariable _) -> Just $ Identifier x
+            Nothing            -> Nothing -- undef
+            _                  -> Nothing -- read to wrong type (function?
+
+        toReadStmt Nothing = Nothing
+        toReadStmt (Just a) = Just $ Rd a
+
+    --                str    expr
+    transWriteStmt :: Ast -> Ast -> Maybe Ast
+    transWriteStmt s e = toWriteStmt $ case transExpr e of
+        Nothing -> Nothing
+        Just (t, a) -> transFormatStr s t <> Just a <> Just []
+        where
+            toWriteStmt Nothing = Nothing
+            toWriteStmt (Just [a, b]) = Just $ Wt a b
+
+    transRetStmt :: Ast -> Maybe Ast
+    transRetStmt a = case transExpr a of
+        Nothing -> Nothing
+        Just (t, x) -> case Map.lookup "" st of
+            Just (SFunction SVoid _) -> case x of
+                Empty -> Just $ Ret Empty
+                _     -> Nothing  -- return a none-void value in void function
+            -- maybe we should make a type conversion here
+            Just (SFunction _ _) -> case x of
+                Empty -> Nothing -- not return a value in a none-void function
+                x     -> Just $ Ret x
+            Nothing -> Nothing -- this is not expected to happen
+ 
+    --          func-name  param-list
+    transFunCall :: Ast -> [Ast] -> Maybe Ast
+    transFunCall (Identifier fn) pl = case Map.lookup fn st of
+        Nothing -> Nothing -- call undefined function
+        Just (SFunction _ pld) -> toFuncCall $ foreach pld pl check
+        where
+            foreach (d:ds) (x:xs) f = f d x <> foreach ds xs f
+            foreach [] [] _ = Just []
+            foreach _  [] _ = Nothing -- param number error
+            foreach [] _  _ = Nothing
+
+            check d x = case transExpr x of
+                Nothing -> Nothing
+                Just (t, a) -> case (d, t) of
+                --  (type-defined, type-passed)
+                    (SInt, SInt) -> Just a
+                    (SChar, SChar) -> Just a
+                    _ -> Just a -- maybe we can make a type conversion here
+
+            toFuncCall Nothing = Nothing
+            toFuncCall (Just x) = Just $ FuncCall (Identifier fn) x
+                    
+    --           left   right
+    transAssign :: Ast -> Maybe Ast
+    transAssign (Assign l r) = case l of
+        (Array _ _) -> case transArray l of
+            Nothing -> Nothing
+            Just x  -> case transExpr r of
+                Nothing -> Nothing
+                -- type conversion maybe
+                Just (t, a) -> Just $ Assign x a
+
+    transArray :: Ast -> Maybe Ast
+    transArray (Array (Identifier n) i) = case Map.lookup n st of
+        Just (SArray _ _) -> case transExpr i of
+            Nothing -> Nothing
+            Just (t, a) -> Just $ Array (Identifier n) a
+        _ -> Nothing -- undefined or wrong type
+
+    transFormatStr :: Ast -> SType -> Maybe Ast
+    -- check format string is boring, i don't want to do it...
+    transFormatStr a _ = Just a
+
+    transExpr :: Ast -> Maybe (SType, Ast)
+    transExpr x = case transExpr' x of
+        Nothing -> Nothing
+        Just (t, v, a) -> case v of
+            EStrictN n -> Just (t, Number n)
+            _ -> Just (t, a)
+
+    transExpr' :: Ast -> Maybe (SType, ExprValue, Ast)
+    transExpr' (BinNode o l r) = bind o (transExpr' l) (transExpr' r)
+        where
+            bind o a b = case flatten a b of
+                Just (EStrictN c, _, EStrictN c', _) -> forOp o c c'
+                Just (_, a, _, a') -> Just (SInt, EVariable, BinNode o a a')
+                Nothing -> Nothing
+
+            flatten (Just (_, v, a)) (Just (_, v', a')) = Just (v, a, v', a')
+            flatten _ _ = Nothing
+
+            forOp Mul a b = putValue a * b
+            forOp Div a b = putValue a / b
+            forOp Add a b = putValue a + b
+            forOp Sub a b = putValue a - b
+            forOp Gt  a b = putValue $ fromEnum a >  b
+            forOp Ls  a b = putValue $ fromEnum a <  b
+            forOp GE  a b = putValue $ fromEnum a >= b
+            forOp LE  a b = putValue $ fromEnum a <= b
+            forOp Equ a b = putValue $ fromEnum a == b
+            forOp Neq a b = putValue $ fromEnum a /= b
+            forOp And a b = putValue $ fromEnum a /= 0 && b /= 0
+            forOp Or  a b = putValue $ fromEnum a /= 0 || b /= 0
+
+            putValue v = Just (SInt, EStrictN v, Number v)
+
+    transExpr' (UnaryNode Not a) = case transExpr' a of
+        Just (_, EStrictN n, _) -> let x = fromEnum n == 0 in Just (SInt ,EStrictN x, Number x)
+        Just (_, ENot, (UnaryNode _ a)) -> Just (SInt, EVariable, a)
+        Just (_, _, a) -> Just (SInt, EVariable, UnaryNode Not a)
+        _ -> Nothing
+            
+                
+
+    -- instance (:) for Maybe
+    infixr 5 <>
+    (<>) :: Maybe a -> Maybe [a] -> Maybe [a]
+    a <> b = case b of
+        Nothing -> Nothing
+        Just xs -> case a of
+            Nothing -> Nothing
+            Just  x -> Just (x:xs)
+        
+
+    
 
 
 
--- Libs/TransAst.hs
+++ Libs/TransAst.hs
@@ -10,12 +10,18 @@ type SymbolTable = Map String Symbol
 data Symbol = STempSymbol
             | SVariable SType
             | SArray SType SSize
-            | SConst SType
+            | SConst SType Int
             | SFunction SType [SType]
             deriving (Show)
 
 data SType = SInt | SChar | SVoid  deriving (Show)
 
+
 type SSize = Int
 
+
+data ExprValue = EStrictN Int
+               | EVariable
+               | ENot
+               deriving (Show)
 
