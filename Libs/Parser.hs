866ab88f8ba75d4e897a99786e2546eccae9347f
QingQiz <sofeeys@outlook.com>
2020-05-04 19:50:31 +0800

    rewrite: rewrite parser in Monad Transformer

--- Libs/Parser.hs
+++ Libs/Parser.hs
@@ -1,44 +1,128 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Parser where
 
 import Data.Char
 import Control.Monad
+import Control.Monad.State
+import Control.Monad.Except
 import Control.Applicative
 
 
+newtype Pos = Pos (Int, Int) deriving (Eq, Ord, Show)
+
+data ParseError = DefaultError String
+                | ErrorWithPos String Pos
+                | EofError Pos
+                deriving (Eq, Ord, Show)
+
+newtype PString = PString (String, Pos)
+
 newtype Parser a = Parser {
-        parse :: String -> Maybe (a, String)
+        -- ExceptT :: m (Either a b) -> ExceptT a m b
+        -- State s a :: s -> (a, s)
+        -- State PString (Either ParseError a) >> PString -> (Either ParseError a, PString)
+        runParser :: ExceptT ParseError (State PString) a
     }
+    deriving (Functor, Applicative, Monad, MonadError ParseError)
 
-instance Functor Parser where
-    -- fmap :: (a -> b) -> f a -> f b
-    -- pa :: Parser a
-    fmap f pa = Parser $ \inp ->
-        case parse pa inp of 
-            Nothing -> Nothing
-            Just (x, xs) -> Just (f x, xs)
-
-instance Applicative Parser where
-    -- pure :: a -> f a
-    pure x = Parser $ \s -> Just (x, s)
-    -- (<*>) :: f (a -> b) -> f a -> f b
-    pab <*> pa = Parser $ \inp ->
-        case parse pab inp of
-            Nothing -> Nothing
-            Just (fab, rst) -> parse (fab <$> pa) rst
-
-instance Monad Parser where
-    -- (>>=) :: m a -> (a -> m b) -> m b
-    pa >>= apb = Parser $ \inp ->
-        case parse pa inp of
-            Nothing -> Nothing
-            Just (a, rst) -> parse (apb a) rst
 
 instance Alternative Parser where
-    -- empty :: f a
-    empty = Parser $ const Nothing
-    -- (<|>) :: f a -> f a -> f a
-    pa <|> pb = Parser $ \inp ->
-        case parse pa inp of
-            Nothing -> parse pb inp
-            x -> x
+    empty = throwError $ DefaultError ""
+    p1 <|> p2 = p1 `catchError` \e1 -> p2 `catchError` \e2 -> throwError e1
+
+
+p1 <?> p2 = (try p1) <|> (try p2)
+
+
+try :: Parser a -> Parser a
+try p = Parser (lift get) >>= \s -> p `catchError` \e -> Parser $ lift (put s) >> throwError e
+
+
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy f = do
+    PString s <- Parser $ lift get
+    case s of
+        ("", p) -> throwError $ EofError p
+        (x:xs, pos@(Pos (a, b)))
+            | f x ->
+                let
+                    pos' = case x of
+                        '\t' -> Pos (a, b + 8)
+                        '\n' -> Pos (a + 1, 1)
+                        _    -> Pos (a, b + 1)
+                in
+                    Parser $ lift (put $ PString (xs, pos')) >> return x
+            | otherwise ->
+                throwError $ ErrorWithPos ("Unexcepted char " ++ show x) pos
+
+
+parse :: Parser a -> String -> Either ParseError a
+parse p s = case (runState . runExceptT . runParser) p (PString (s, Pos (1, 1))) of
+    (Left err, _) -> Left (DefaultError $ showErr err s)
+    (Right r,  _) -> Right r
+
+
+showErr :: ParseError -> String -> String
+showErr e inp = case e of
+    DefaultError s              -> s
+    EofError       (Pos (a, b)) -> show a ++ ":" ++ show b ++ ": \x1b[91merror\x1b[0m: End of Input"
+    ErrorWithPos s (Pos (a, b)) -> let n = length $ show a in
+        show a ++ ":" ++ show b ++ ": \x1b[91merror\x1b[0m:\n - " ++ s ++ "\n" ++
+        replicate n ' ' ++ " \x1b[94m|\n" ++
+        show a ++ " | \x1b[0m" ++ lines inp !! (a - 1) ++ "\n" ++
+        replicate n ' ' ++ " \x1b[94m|\x1b[91m " ++ replicate (b - 1) ' ' ++ "^\x1b[0m"
+
+
+catchPErr :: Parser a -> String -> Parser a
+catchPErr pa s = pa `catchError` \err -> case err of
+    ErrorWithPos e p -> throwError $ ErrorWithPos (e ++ "\n - " ++ s) p
+    a                -> throwError a
+
+
+satOrError :: (Char -> Bool) -> String -> Parser Char
+satOrError f s = catchPErr (satisfy f) s
+
+
+putErr (Left (DefaultError a)) = putStrLn a
+
+------------------------------------------------------------------
+--                 some instances of Parser                     --
+------------------------------------------------------------------
+char c   = satOrError (==c)       $ "Excepted char: " ++ show c
+space    = satOrError isSpace     $ "Excepted a space"
+digit    = satOrError isDigit     $ "Excepted a digit"
+oneOf cs = satOrError (`elem` cs) $ "Excepted one char of [" ++ cs ++ "]"
+
+string str = string' str `catchPErr` ("Excepted string: " ++ show str) where
+    string' "" = return ""
+    string' str@(x:xs) = (char x >> string' xs >> return str)
+
+uInt :: Parser Int
+uInt = (fmap read $ ((:) <$> (many space >> (oneOf "123456789")) <*> many digit) <?> string "0")
+    `catchPErr` "Excepted a unsigned int"
+
+sInt :: Parser Int
+sInt = (string "+" <?> string "-" <?> string "") >>= toInt (many space >> uInt) where
+    toInt ps c | c == "-" = negate <$> ps
+               | otherwise = ps
+
+spcChar inp = many space >> char inp
+spcStr  inp = many space >> string inp
+
+-- parse `a sep a sep a' or `a'
+sepBy sep a = (:) <$> a <*> many (sep >> a)
+
+-- parse `a sep a sep a' or `a' or `'
+sepByE sep a = sepBy sep a <?> return []
+
+-- parse `((a op b) op c)'
+chainl :: Parser (a -> a -> a) -> Parser a -> Parser a
+chainl op p = (many space >> p) >>= for_rest where
+    for_rest x = ((many space >> op) <*> return x <*> p >>= for_rest) <?> return x
+
+-- parse `op (op (op a))'
+unaryOpChain:: Parser (a -> a) -> Parser a -> Parser a
+unaryOpChain op p = (many (many space >> op)) >>= trans p where
+    trans x fs = foldr (\f z -> f <$> z) x fs
 
