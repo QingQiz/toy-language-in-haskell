369467d3f98063254f81d87e479ec879e9dd7244
QingQiz <sofeeys@outlook.com>
2020-05-05 22:45:02 +0800

    chore: fix main.hs

--- Main.hs
+++ Main.hs
@@ -1,26 +1,24 @@
 module Main where
 
 import Ast
+import Parser
 import Symbol
 import Grammar
 import Semantic
 import CodeGen
 
+import System.Exit
 import System.Environment
 
-get_ast :: Maybe (Ast, String) -> Ast
-get_ast (Just (a, b)) = case b of
-    "" -> a
-    _  -> error $ "Parse Error On:\n" ++ b
 
-get_ast' :: Maybe Ast -> Ast
 get_ast' (Just a) = a
 get_ast' Nothing = error $ "Semantic error"
 
 run (fn:xs) = do
     s <- readFile fn
-    let res = unlines $ cProgram $ get_ast' $ semaProgram $ get_ast $ build_ast s
-    putStrLn $ res
+    case buildAst s of
+        Right a -> putStrLn $ unlines $ runCodeGen $ get_ast' $ runSema a
+        Left (DefaultError a) -> die(a)
 
 main :: IO ()
 main = do
