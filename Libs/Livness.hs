eb7288d5e0cbcce0e8abee887084e35ba483fc28
QingQiz <sofeeys@outlook.com>
2020-06-12 17:26:00 +0800

    refactor: move some functions from Optimizer to Livness and TAC

---
+++ Libs/Livness.hs
@@ -0,0 +1,107 @@
+module Livness where
+
+import Register
+import Functions
+import Data.List
+import Data.List.Split
+import qualified Data.Map as Map
+
+
+livnessAnalysis tacs ids entries =
+    let
+        g_vars = filter (\x -> "rip" `isInfixOf` x)
+            $ concat
+            $ map (\(a, b) -> getRegs a ++ getRegs b)
+            $ concat tacs
+        id_liv = (\inp -> Map.insert (last ids) (getItem (last ids) inp ++ ["%rax", "%rsp", "%rbp"] ++ g_vars) inp)
+            $ Map.fromList
+            $ zipWith findGV ids
+            $ replicate (length ids) []
+
+        findGV id _ = (id, findGVar (getItem id id_tac)) where
+            findGVar ((a, b):ts)
+                | "rbx" `isInfixOf` a && "rip" `isInfixOf` b && head b /= '*' =
+                      ("*(%rbx)") : findGVar ts
+                | otherwise = findGVar ts
+            findGVar _ = []
+    in
+        untilNoChange (buildLiveness (head tacs) (head ids) (head entries) []) id_liv
+    where
+        id_entry_t = zip ids entries
+        id_entry   = Map.fromList id_entry_t
+        id_tac     = Map.fromList $ zip ids tacs
+
+        buildLiveness tac id [] vis id_liv = updateLiv tac id id_liv
+
+        buildLiveness tac id entry vis id_liv =
+            let (id_liv', _) = foldr step (id_liv, vis) entry where
+                    step x z@(id_liv, vis) = if x `elem` vis then z else
+                        let tac     = getItem x id_tac
+                            entry   = getItem x id_entry
+                            id_liv' = buildLiveness tac x entry (x:vis) id_liv
+                        in (id_liv', x:vis)
+            in updateLiv tac id id_liv'
+
+        updateLiv tac id id_liv =
+            let liv      = collectLivness tac $ getItem id id_liv
+                push_up  = if null liv then [] else map rmRegIndex $ head liv
+                upstream = find_upstream id
+                id_liv'  = foldr (\id m -> updateLiv' id push_up m) id_liv upstream
+            in  id_liv'
+            where
+                updateLiv' id l id_liv =
+                    let x = l ++ getItem id id_liv in Map.insert id (rmDupItem x) id_liv
+
+        find_upstream id = map fst $ filter (\x -> id `elem` snd x) id_entry_t
+
+
+collectLivness tac liv = foldr step [liv] tac where
+    step x z = (getLivness x (head z)) : z
+
+    getLivness (a, b) init
+        | head a == 'j'        = init
+        | a == "cmp"           = getRegs b ++ init
+        | "set" `isPrefixOf` a = specialRemove b init
+        | a == "pushq"         = getRegs b ++ init
+        | a == "call"          = let n =  read $ last $ splitOn "#" b
+                                 in  "%rax" : (take n $ tail $ tail $ map (!!0) registers) ++ init
+        | a == "cltd"          = "%rax" : init
+        | a == "cltq"          = "%rax" : init
+        | a == "cqto"          = "%rax" : init
+        | otherwise            = (if isRegGroup a then tail $ getGroupVal a else [])
+                                 ++ (if head a == '*' then getRegs $ tail a else [])
+                                 ++ getRegs b
+                                 ++ specialRemove a init
+
+    specialRemove a l = let fixed_reg = rmRegIndex a in
+        removeWhere (\x -> x == fixed_reg || x == a) l
+
+
+fixRegIndexInLiv tac liv =
+    let m_init = Map.fromList $ zip regs [0,0..]
+        regs   = (["%rsp", "%rbp"] ++ map (!!0) registers)
+    in  fixLiv' (("",""):tac) liv m_init
+    where
+        fixLiv' (t:tac) (l:liv) m =
+            let (l', m') = foldr step ([], m) (rmDupItem l)
+            in l' : fixLiv' tac liv m'
+            where step x (res, m) =
+                    let (r', m') = fixRegIndex t x m
+                    in  (r':res, m')
+        fixLiv' [] [] _ = []
+
+        getItem' x m = case Map.lookup x m of {Nothing -> 0; Just x -> x}
+
+        fixRegIndex (a, b) r m =
+            let regs = getRegs a ++ getRegs b
+                m'   = foldr (\x z -> updateM x z) m regs
+            in  if getRegIndex r == ""
+                then (r ++ (show $ getItem' r m'), m')
+                else (r, m')
+
+        updateM r m = let r' = rmRegIndex r
+                          idx_new = getRegIndex r
+                          idx_now = getItem' r' m
+                      in  if (read idx_new) > idx_now
+                          then Map.insert r' (read idx_new) m
+                          else m
